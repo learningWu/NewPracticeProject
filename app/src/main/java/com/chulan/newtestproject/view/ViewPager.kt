b197d5f04c79c9581fcd727a4140b33340cf4314
wuzixuan <wuzixuan@changingedu.com>
2021-05-12 23:58:00 +0800

    【ViewPager】init

---
+++ app/src/main/java/com/chulan/newtestproject/view/ViewPager.kt
@@ -0,0 +1,16 @@
+package com.chulan.newtestproject.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class ViewPager @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : ViewGroup(context, attrs, defStyleAttr) {
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+}
